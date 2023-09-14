78e782ffc7942cf0cab3b78cdbef4e9783cef743
Rey Chen <rachel.s.chen@protonmail.com>
2023-09-14 13:42:54 -0700

    init

---
+++ Main.gd
@@ -0,0 +1,74 @@
+extends Node
+
+@export var mob_scene: PackedScene # Enables assignment of tscn file to variable
+var score
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+# Player's hit signal is connected here as game_over.
+func game_over(): 
+	$ScoreTimer.stop()
+	$MobTimer.stop()
+	$HUD.show_game_over()
+	$Music.stop()
+	$DeathSound.play()
+
+
+# Connected Hud.gd's start_game signal to Main.gd's new_game func.
+func new_game():
+	score = 0
+	
+	$Player.start($StartPosition.position)
+	$StartTimer.start()
+	
+	$HUD.update_score(score)
+	$HUD.show_message("Get ready")
+	
+	get_tree().call_group("mobs", "queue_free")
+	
+	$Music.play()
+
+
+func _on_score_timer_timeout():
+	score += 1
+	$HUD.update_score(score)
+
+
+func _on_start_timer_timeout():
+	$MobTimer.start()
+	$ScoreTimer.start()
+
+
+func _on_mob_timer_timeout():
+	# Create a new instance of the Mob scene.
+	var mob = mob_scene.instantiate()
+	
+	# Choose a random location on Path2D.
+	# You drew it along the border of the screen, which probably means it'll spawn somewhere at the border.
+	var mob_spawn_location = get_node("MobPath/MobSpawnLocation")
+	mob_spawn_location.progress_ratio = randf()
+	
+	# Set the mob's direction perpendicular to the path direction.
+	var direction = mob_spawn_location.rotation + PI/2
+	
+	# Set the mob's position to the chosen random location.
+	mob.position = mob_spawn_location.position
+
+	# Add some randomness to the starting direction.
+	direction += randf_range(-PI/4, PI/4)
+	mob.rotation = direction
+	
+	# Choose the velocity for the mob.
+	var velocity = Vector2(randf_range(150.0, 250.0), 0.0)
+	mob.linear_velocity = velocity.rotated(direction) # Converts angle to euclidean?
+	
+	# Spawn the mob by adding it to the Main scene.
+	add_child(mob)
